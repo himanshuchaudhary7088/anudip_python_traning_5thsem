9f4c77f653e7682e9b478ec699d5e81ba2c30838
himanshu chaudhary <himanshuchaudhary7088@gmail.com>
2026-06-03 15:59:39 +0530

    python

---
+++ CLASSROOM/tpes_of_triangle.py
@@ -0,0 +1,30 @@
+#program to check three angles form a triangle
+angle1 = float(input("enter the first angle: "))
+#validate angle 1
+if(angle1<0):
+    exit("angle cannot be negative")
+#........................................................
+angle2 = float(input("enter the second angle: "))
+#validate angle 2
+if(angle2<0):
+    exit("angle cannot be negative")
+#..............................................................    
+angle3 = float(input("enter the third angle: "))
+#validate angle 3
+if(angle3<0):
+    exit("angle cannot be negative")
+#...........................................................
+#veryfing triangle formation
+if(angle1+angle2+angle3 == 180):
+
+    print("triangle can form ")
+#triangle formed 
+# type of triangle
+    if(angle1 < 90 and angle2 < 90 and angle3 < 90):
+       print("formed angle is acute angle triangle")
+    elif(angle1==90 or angle2==90 or angle3==90):
+       print("formed angle is right angle triangle")
+    else:
+       print("formed angle is obtuse angle triangle")   
+else:
+    print("triangle doesn't form")
