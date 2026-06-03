fd55309b23478708a359adbe992f4d72513fb73b
himanshu chaudhary <himanshuchaudhary7088@gmail.com>
2026-06-03 15:38:38 +0530

    python

---
+++ SELF-LEARNING/triangle_formation_by_side.py
@@ -0,0 +1,20 @@
+# Enter and validate first side
+side1 = float(input("Enter first side: "))
+if side1 <= 0:
+    exit("First side must be greater than 0")
+
+# Enter and validate second side
+side2 = float(input("Enter second side: "))
+if side2 <= 0:
+    exit("Second side must be greater than 0")
+
+# Enter and validate third side
+side3 = float(input("Enter third side: "))
+if side3 <= 0:
+    exit("Third side must be greater than 0")
+
+# Verify triangle formation
+if (side1 + side2 > side3) and (side1 + side3 > side2) and (side2 + side3 > side1):
+    print("Triangle can be formed")
+else:
+    print("Triangle cannot be formed")
